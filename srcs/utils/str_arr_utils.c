25423f06dc0d16d8ffbf77c2894c86c51a05d946
scoach <scoach@student.21-school.ru>
2022-03-04 13:55:35 +0300

    4.03 Egor

--- srcs/utils/str_arr_utils.c
+++ srcs/utils/str_arr_utils.c
@@ -6,7 +6,7 @@
 /*   By: scoach <scoach@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 16:44:14 by scoach            #+#    #+#             */
-/*   Updated: 2022/02/21 18:59:00 by scoach           ###   ########.fr       */
+/*   Updated: 2022/03/04 13:54:54 by scoach           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,45 +103,4 @@ char	**ft_arrjoin(char **arr1, char **arr2)
 		return (ft_free_arr(final_arr, i));
 	final_arr[ln - 1] = NULL;
 	return (final_arr);
-}
-
-static char	**ft_strtoarr(char *str)
-{
-	char	**arr;
-
-	arr = malloc(sizeof(char *) * 2);
-	if (arr == NULL)
-		return (NULL);
-	arr[0] = ft_strdup(str);
-	if (arr[0] == NULL)
-		return (ft_free_arr(arr, 0));
-	arr[1] = NULL;
-	return (arr);
-}
-
-char	**ft_arr_plus_one(char ***sub_arr, char *str, int i, int ln)
-{
-	char	**tmp;
-	char	**tmp2;
-	char	*substr;
-
-	if (ln <= 0)
-		return (*sub_arr);
-	tmp = ft_arrdup(*sub_arr);
-	ft_free_arr(*sub_arr, ft_arrlen(*sub_arr));
-	if (tmp == NULL)
-		return (NULL);
-	substr = ft_substr(str, i, ln);
-	if (substr == NULL)
-		return (ft_free_arr(tmp, ft_arrlen(tmp)));
-	tmp2 = ft_strtoarr(substr);
-	free(substr);
-	if (tmp2 == NULL)
-		return (ft_free_arr(tmp, ft_arrlen(tmp)));
-	(*sub_arr) = ft_arrjoin(tmp, tmp2);
-	ft_free_arr(tmp, ft_arrlen(tmp));
-	ft_free_arr(tmp2, ft_arrlen(tmp2));
-	if (*sub_arr == NULL)
-		return (NULL);
-	return (*sub_arr);
 }
